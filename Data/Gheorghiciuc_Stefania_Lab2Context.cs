245f7b65795d4ebe2703dfcd4ccb987c23cb107f
stefaniagheorghiciuc <sgheorghiciuc@gmail.com>
2025-10-19 23:26:51 +0300

    Add project files.

---
+++ Data/Gheorghiciuc_Stefania_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gheorghiciuc_Stefania_Lab2.Models;
+
+namespace Gheorghiciuc_Stefania_Lab2.Data
+{
+    public class Gheorghiciuc_Stefania_Lab2Context : DbContext
+    {
+        public Gheorghiciuc_Stefania_Lab2Context (DbContextOptions<Gheorghiciuc_Stefania_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gheorghiciuc_Stefania_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Gheorghiciuc_Stefania_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Gheorghiciuc_Stefania_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Gheorghiciuc_Stefania_Lab2.Data;
+using Gheorghiciuc_Stefania_Lab2.Models;
+
+namespace Gheorghiciuc_Stefania_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Gheorghiciuc_Stefania_Lab2.Data.Gheorghiciuc_Stefania_Lab2Context _context;
+
+        public CreateModel(Gheorghiciuc_Stefania_Lab2.Data.Gheorghiciuc_Stefania_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "LastName");
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
